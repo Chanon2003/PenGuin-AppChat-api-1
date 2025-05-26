dabb695a6062ba22b40c0e052ae816cee2e28848
Chanon <CH.Chanonlienwork@gmail.com>
2025-05-26 16:16:24 +0700

    80per sigun up controller

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import mongoose from 'mongoose'
 import connectDB from './db/connect.js'
+import errorHandlerMiddleware from './middlewares/error-handler.js'
+import authRoutes from './routes/AuthRoutes.js'
 
 dotenv.config();
-
-const app = express()
 const port = process.env.PORT || 3001
+
+//middleware
+const app = express()
 
 app.use(cors({
   origin:[process.env.ORIGIN],
@@ -16,6 +18,13 @@ app.use(cors({
   credentials:true
 }))
 
+app.use(cookieParser());
+app.use(express.json())
+
+//route
+app.use('/api/auth',authRoutes)
+
+//connect Database
 const start = async()=>{
   try {
     await connectDB(process.env.DATABASE_URL)
@@ -24,12 +33,9 @@ const start = async()=>{
   }
 }
 
-app.use(cookieParser());
-app.use(express.json())
-
 start()
 
-
+app.use(errorHandlerMiddleware)
 
 const server = app.listen(port,()=>{
   console.log(`Server is Running ON Port : ${port}`)
